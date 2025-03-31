31a911d9d722ab19346d59bf8605f921643df5e4
Victor Ferreira <vitfera@hotmail.com>
2025-03-31 17:28:53 -0300

    logo e textos

--- docker/common/config.d/0.main.php
+++ docker/common/config.d/0.main.php
@@ -1,7 +1,7 @@
 <?php 
 
 return [
-    'app.siteName' => 'Mapas Culturais Base Project',
+    'app.siteName' => 'CulturAZ Santo André',
     'app.siteDescription' => 'O Mapas Culturais é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e oportunidades',
     
     // Define o tema ativo no site principal. Deve ser informado o namespace do tema e neste deve existir uma classe Theme.
