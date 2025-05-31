99c0f1de6c1123fd551a991cbd0c1ca9cfdc6d4f
Victor Ferreira <vitfera@hotmail.com>
2025-05-31 01:56:17 -0300

    plugins de download

--- docker/common/config.d/plugins.php
+++ docker/common/config.d/plugins.php
@@ -4,5 +4,7 @@ return [
     'plugins' => [
         'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ],
         'SamplePlugin' => ['namespace' => 'SamplePlugin'],
+        //'RegistrationFiles' => ['namespace' => 'RegistrationFiles'],
+        'DownloadFilesRegistrations' => ['namespace' => 'DownloadFilesRegistrations'],
     ]
 ];
